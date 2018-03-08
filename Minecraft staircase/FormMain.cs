e965f21aa16c5099f78d0de16813887ce49d2f39
Михаил <sokolmish@yandex.ru>
2018-03-08 22:19:05 +0300

    1.3.1.0

--- Minecraft staircase/FormMain.cs
+++ Minecraft staircase/FormMain.cs
@@ -2,9 +2,6 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
-using System.IO;
-using System.Web.Script.Serialization;
-using System.Text;
 using System.Threading;
 using System.Resources;
 using Substrate;
