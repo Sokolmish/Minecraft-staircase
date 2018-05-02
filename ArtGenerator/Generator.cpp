1f3d977d4f92fc07739f2167f19342bc0aadc503
Михаил <sokolmish@yandex.ru>
2018-05-02 03:12:30 +0300

    CPP converting

---
+++ ArtGenerator/Generator.cpp
@@ -0,0 +1,82 @@
+#include <string>
+#include <vector>
+
+#define DARK 0
+#define NORM 1
+#define LIGH 2
+
+#define FLAT 0
+#define LITE 1
+#define FULL 2
+
+double Similarity(int *col1, int *col2, int set) {
+	switch (set) {
+	case LIGH:
+		return sqrt(pow(col2[0] - col1[0], 2) +
+			pow(col2[1] - col1[1], 2) +
+			pow(col2[2] - col1[2], 2));
+	case NORM:
+		return sqrt(pow(col2[0] * 220 / 255 - col1[0] * 220 / 255, 2) +
+			pow(col2[1] * 220 / 255 - col1[1] * 220 / 255, 2) +
+			pow(col2[2] * 220 / 255 - col1[2] * 220 / 255, 2));
+	case DARK:
+		return sqrt(pow(col2[0] * 180 / 255 - col1[0] * 180 / 255, 2) +
+			pow(col2[1] * 180 / 255 - col1[1] * 180 / 255, 2) +
+			pow(col2[2] * 180 / 255 - col1[2] * 180 / 255, 2));
+	default:
+		return 0;
+	}
+}
+
+double* RGBtoXYZ(int x1, int y1, int z1) {
+	return new double[3]{
+		0.4124564 * x1 + 0.3575761 * y1 + 0.1804375 * z1,
+		0.2126729 * x1 + 0.7151522 * y1 + 0.0721750 * z1,
+		0.0193339 * x1 + 0.1191920 * y1 + 0.9503041 * z1
+	};
+}
+
+__declspec(dllexport)
+int* Convert(int *image/*r-g-b*/, int length, int type, bool chromatic, int *notes/*=id-rgb=*/, int colCount, void(*Progress)(), void(*SaveUses)(int cou))
+{
+	//>>r1,g1,b1,r2,g2,b2, ri,gi,bi
+	//<<id1,set1,id2,set2, idi,seti
+
+	int* result = /*(int*)malloc(length / 3 * 2);*/new int[length / 3 * 2];
+	int* uses = /*(int*)malloc(colCount / 4);*/new int[colCount / 4];
+	for (int i = 0; i < length / 3; i++) {
+		int betterSimilarity = 99999;
+		int betterId = 0;
+		int betterSet = NORM;
+		int betterIdNum = 0;
+		for (int col = 0; col < colCount / 4; col++) {
+			if (Similarity(&notes[col * 4 + 1], &image[i * 3], NORM) < betterSimilarity) {
+				betterSimilarity = Similarity(&notes[col * 4 + 1], &image[i * 3], NORM);
+				betterId = notes[col * 4];
+				betterSet = NORM;
+				betterIdNum = col;
+			}
+			if (type != FLAT) {
+				if (Similarity(&notes[col * 4 + 1], &image[i * 3], LIGH) < betterSimilarity) {
+				betterSimilarity = Similarity(&notes[col * 4 + 1], &image[i * 3], LIGH);
+				betterId = notes[col * 4];
+				betterSet = LIGH;
+				betterIdNum = col;
+				}
+				if (type != LITE)
+					if (Similarity(&notes[col * 4 + 1], &image[i * 3], DARK) < betterSimilarity) {
+						betterSimilarity = Similarity(&notes[col * 4 + 1], &image[i * 3], DARK);
+						betterId = notes[col * 4];
+						betterSet = DARK;
+						betterIdNum = col;
+					}
+			}
+		}
+		result[i * 2] = betterId;
+		result[i * 2 + 1] = betterSet;
+		//++uses[betterIdNum];
+		Progress();
+	}
+	//SaveUses(uses);
+	return result;
+}
--- Minecraft staircase/ArtGenerator.cs
+++ Minecraft staircase/ArtGenerator.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace Minecraft_staircase
 {
-    class ArtGenerator
+    unsafe class ArtGenerator
     {
         List<ColorNote> _colors;
 
@@ -20,12 +21,13 @@ namespace Minecraft_staircase
 
         public SettedBlock[,] CreateScheme(ref Image sourceImage, ArtType type, out int maxHeight)
         {
-            UnsettedBlock[,] RawScheme = Convert(ref sourceImage, type);
+            UnsettedBlock[,] RawScheme = ConvertCPP(ref sourceImage, type);
+            //UnsettedBlock[,] RawScheme = OldConvert(ref sourceImage, type);
             return GenerateFlow(ref RawScheme, out maxHeight);
         }
 
 
-        UnsettedBlock[,] Convert(ref Image sourceImage, ArtType type)
+        UnsettedBlock[,] OldConvert(ref Image sourceImage, ArtType type)
         {
             UnsettedBlock[,] RawScheme = new UnsettedBlock[sourceImage.Width, sourceImage.Height];
             Bitmap tempImage = sourceImage as Bitmap;
@@ -187,6 +189,53 @@ namespace Minecraft_staircase
         public void SetProgress(ProgressBar progress)
         {
             this.progress = progress;
+        }
+
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate void Progress();
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate void SaveUses(int cou);
+        [DllImport("ArtGenerator.dll", CallingConvention = CallingConvention.Cdecl)]
+        static extern int* Convert(int* image, int imageLen, int type, bool chromatic, int* notes, int notesCount, [MarshalAs(UnmanagedType.FunctionPtr)] Progress callback, [MarshalAs(UnmanagedType.FunctionPtr)] SaveUses callback1);
+
+        UnsettedBlock[,] ConvertCPP(ref Image sourceImage, ArtType type)
+        {
+            UnsettedBlock[,] result = new UnsettedBlock[sourceImage.Width, sourceImage.Height];
+            List<int> image1 = new List<int>(sourceImage.Width * sourceImage.Height * 3);
+            for (int i = 0; i < sourceImage.Height; i++)
+                for (int j = 0; j < sourceImage.Width; j++)
+                {
+                    image1.Add((sourceImage as Bitmap).GetPixel(j, i).R);
+                    image1.Add((sourceImage as Bitmap).GetPixel(j, i).G);
+                    image1.Add((sourceImage as Bitmap).GetPixel(j, i).B);
+                }
+            fixed (int* image = image1.ToArray())
+            {
+                List<int> notes1 = new List<int>(_colors.Count * 4);
+                foreach (ColorNote col in _colors)
+                {
+                    notes1.Add(col.ColorID);
+                    notes1.Add(col.LightColor.R);
+                    notes1.Add(col.LightColor.G);
+                    notes1.Add(col.LightColor.B);
+                }
+                fixed (int* notes = notes1.ToArray())
+                {
+                    int* fet = 
+                    Convert(image, image1.Count, 0, false, notes, notes1.Count,
+                        () => { progress.BeginInvoke(Inc); }, /*MessageBox.Show("Soul eather");*/
+                        (e) => { MessageBox.Show(e.ToString()); });
+                    int i = 0;
+                    for (int x = 0; x < sourceImage.Height; x++)
+                        for (int y = 0; y < sourceImage.Width; y++)
+                        {
+                            result[y, x].ID = fet[i++];
+                            result[y, x].Set = (ColorType)fet[i++];
+                        }
+                }
+            }
+            return result;
         }
     }
 }
